a5bc5e6346d2d752d15b8b2e4608e243d134f712
borikim <naino97@gmail.com>
2024-03-06 20:51:49 +0900

    day 1

---
+++ Cpp/programmers_basic4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    string str = "";
+    cin >> str;
+
+    if(str.length() < 1 || str.length() > 20){
+        return -1;
+        }
+    
+    vector<char> alphabet(str.begin(), str.end());
+
+    for(char msg : alphabet){
+        if(isupper(msg)){
+            cout << char(tolower(msg));
+        }
+        else if(islower(msg)){
+            cout << char(toupper(msg));
+        }
+    }
+    
+    return 0;
+}
