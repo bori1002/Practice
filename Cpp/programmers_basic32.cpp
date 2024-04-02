553e92cb94174c6dfbcf85b80e279fe0fdee09e7
borikim <naino97@gmail.com>
2024-04-02 23:20:11 +0900

    day8 -ing

--- Cpp/programmers_basic32.cpp
+++ Cpp/programmers_basic32.cpp
@@ -3,7 +3,25 @@
 
 using namespace std;
 
-int min_num(vector<int> num_list){
+int case1(int p){
+    return 1111 * p;
+}
+int case2(int p, int q){
+    return (10 * p + q) * (10 * p + q);
+}
+int case3(int p, int q){
+    if(p > q){
+        return (p + q) * (p - q);
+    }
+    else if( p < q){
+        return (p + q) * (q - p);
+    }
+}
+int case4(int q, int r){
+    return q * r;
+}
+int case5(int a, int b, int c, int d){
+    vector<int> num_list = {a, b, c, d};
     int temp = num_list[0];
     int minValue = 0;
     for(const int& num : num_list){
@@ -15,49 +33,10 @@ int min_num(vector<int> num_list){
 
 int solution(int a, int b, int c, int d){
     int answer = 0;
-    vector<int> num_list = {a, b, c, d}; 
-    vector<bool> CASE;
-
-    if(a == b){
-        CASE.push_back(1);
-    }
-    else if(a != b){
-        CASE.push_back(0);
-    }
-
-    if(a == c){
-        CASE.push_back(1);
-    }
-    else if(a != c){
-        CASE.push_back(0);
-    }
-
-    if(a == d){
-        CASE.push_back(1);
-    }
-    else if(a != d){
-        CASE.push_back(0);
-    }
-
-    if(b == c){
-        CASE.push_back(1);
-    }
-    else if(b != c){
-        CASE.push_back(0);
-    }
-
-    if(b == d){
-        CASE.push_back(1);
-    }
-    else if(b != d){
-        CASE.push_back(0);
-    }
-
-    if(c == d){
-        CASE.push_back(1);
-    }
-    else if(c != d){
-        CASE.push_back(0);
+    int checkpoint = 0;
+    vector<vector<int>> numlist = {{a, b}, {a, c}, {a, d}, {b, c}, {b, d}, {c, d}};
+    for(vector<int> num : numlist){
+        if(num[0] == num[1])
     }
 
     return answer;
