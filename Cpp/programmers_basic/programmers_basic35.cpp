adf8075153785caac7f3c2d8fd131481a92236e0
kimbori <kimbori@125-0-130-170.staticrdns.enxstatic.com>
2024-05-10 18:04:30 +0900

    finish day8

---
+++ Cpp/programmers_basic/programmers_basic35.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, vector<vector<int>> queries) {
+    string answer = my_string;
+    for(const vector<int> query : queries){
+        int i = 0;
+        for(int s = query[0]; s <= query[1]; s++){
+            if(s >= query[1] - i) break;
+            int temp = answer[s];
+            answer[s] = answer[query[1] - i];
+            answer[query[1] - i] = temp;
+            i++;
+            cout << answer << endl;
+        }
+    }
+    return answer;
+}
+
+int main(){
+    string answer = solution("rermgorpsam", {{2, 3}, {0, 7}, {5, 9}, {6, 10}});
+    cout << answer;
+    return 0;
+}
