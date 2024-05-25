fe6148b5b67e7b3b6508cf9f99106044307c732b
kimbori <kimbori@kimborilaptop.local>
2024-05-25 21:35:38 +0900

    day 13

--- Cpp/programmers_basic/day13/programmers_basic61.cpp
+++ Cpp/programmers_basic/day13/programmers_basic61.cpp
@@ -4,31 +4,16 @@
 
 using namespace std;
 
-vector<int> solution(vector<int> arr, vector<int> query) {
+vector<int> solution(vector<int> num_list, int n) {
     vector<int> answer;
-    vector<int> temp = arr;
-    for(int i = 0; i < query.size(); i++){
-        if(i % 2 == 0){
-            vector<int> new_temp = temp;
-            for(int j = (temp.size()-1); j > query[i]; j--){
-                new_temp.pop_back();
-            }
-            temp = new_temp;
-        }
-        else{
-            vector<int> new_temp = temp;
-            for(int j = 0; j < query[i]; j++){
-                new_temp.erase(new_temp.begin());
-            }
-            temp = new_temp;
-        }
+    for(int i = n-1; i < num_list.size(); i++){
+        answer.push_back(num_list[i]);
     }
-    answer = temp;
     return answer;
 }
 
 int main(){
-    vector<int> answer = solution({0,1,2,3,4,5}, {4,1,2});
+    vector<int> answer = solution({2, 1, 6}, 3);
     for(const int num : answer){
         cout << num << endl;
     }
