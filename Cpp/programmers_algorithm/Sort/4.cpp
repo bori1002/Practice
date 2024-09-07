5c4768aa0dcb90192455e427e8fb4c5fd99990fd
kimbori <kimbori@kimborilaptop.local>
2024-09-07 17:15:55 +0900

    sort 4

---
+++ Cpp/programmers_algorithm/Sort/4.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    //먼저 내림차순으로 정리해서 가장 큰 수부터 탐색
+    sort(citations.begin(), citations.end(), [](const int &a, const int &b){return a > b;});
+    //조건에 만족하는 순간 answer return
+    for(const int citation : citations){
+        //citation 횟수 이상이라면 answer에 저장 후 다음 인용횟수로 넘어가기
+        int count = 0;
+        for(const int h : citations){
+            if(h >= citation){
+                count++;//현재 논문의 인용횟수 h 이상일 경우 count 수를 늘린다.
+                }
+            if(count == citation){
+                answer = citation;
+                return answer;
+                }
+        }
+    }
+    return answer;
+}
+
+/*
+H-Index는 과학자의 생산성과 영향력을 나타내는 지표입니다. 어느 과학자의 H-Index를 나타내는 값인 h를 구하려고 합니다. 위키백과1에 따르면, H-Index는 다음과 같이 구합니다.
+
+어떤 과학자가 발표한 논문 n편 중, h번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용되었다면 h의 최댓값이 이 과학자의 H-Index입니다.
+
+어떤 과학자가 발표한 논문의 인용 횟수를 담은 배열 citations가 매개변수로 주어질 때, 이 과학자의 H-Index를 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+과학자가 발표한 논문의 수는 1편 이상 1,000편 이하입니다.
+논문별 인용 횟수는 0회 이상 10,000회 이하입니다.
+*/
