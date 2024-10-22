5343bcf23842629d8ac6561a8395ccf68ee5eb1f
borikim <naino97@gmail.com>
2024-10-22 13:57:00 +0900

    delete mac

---
+++ Cpp/baekjoon/10.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    uint8_t n;
+
+    for(int i = 1; i < 10; i++){
+        cout << n << " * " << i << n*i << endl;
+    }
+
+    cin >> n;
+    return 0;
+}
---
+++ Cpp/baekjoon/11.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int t = 0;
+    vector<vector<int>> nums;
+    cin >> t;
+    for(int i = 0; i  < t; i++){
+        cin >> a >> b;
+        nums[i].push_back(a);
+        nums[i].push_back(b);
+    }
+    
+}
